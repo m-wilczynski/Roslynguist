d691a65f5ecd084c72bbfa3e3160e526160e3096
Michał Wilczyński <michal.e.wilczynski@gmail.com>
2016-12-04 21:11:58 +0100

    Minor changes to analyzer's dictionary creating.

--- src/Roslynguist/Analyzers/MethodAnalyzers/MethodCallAnalyzer.cs
+++ src/Roslynguist/Analyzers/MethodAnalyzers/MethodCallAnalyzer.cs
@@ -1,14 +1,24 @@
 ﻿namespace Roslynguist.Analyzers.MethodAnalyzers
 {
+    using System;
     using System.Collections.Generic;
     using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+    using Models;
 
     public class MethodCallAnalyzer : SyntaxAnalyzer
     {
+        private readonly Dictionary<IMethodSymbol, MethodModel> _methods = new Dictionary<IMethodSymbol, MethodModel>();
+        private readonly Dictionary<IFieldSymbol, FieldInitializerSyntax> _fields = new Dictionary<IFieldSymbol, FieldInitializerSyntax>();
+        private readonly Dictionary<IPropertySymbol, PropertyBlockSyntax> _properties = new Dictionary<IPropertySymbol, PropertyBlockSyntax>();
+
+        public readonly AnalysisAggregateSource AnalysisSource;
 
         public MethodCallAnalyzer(AnalysisAggregateSource source) : base(source)
         {
-
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            AnalysisSource = source;
         }
 
         public bool WasMapBuilded { get; private set; }
